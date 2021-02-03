644a49f6b424e06c47b50060583e8b54ae1a4e47
Kai Sun <704725181@qq.com>
2021-02-03 16:21:47 +1100

    fix

--- send_offloading_result.py
+++ send_offloading_result.py
@@ -3,6 +3,7 @@ import sys
 def send_back(back_url:str,file_name:str):
     files = {'offloading_file':open(file_name,'rb')}
     r = requests.post(back_url,files = files)
+    print(r.text)
 
 if __name__ == '__main__':
     send_back(sys.argv[1],sys.argv[2])
