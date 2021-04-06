9d194d2108b2dda5aeffd5973036762e26f8634d
孙楷 (Kai) <ksun0017@qq.com>
2021-04-06 22:10:11 +1000

    bug fix

--- latency_awareness/node_manager.py
+++ latency_awareness/node_manager.py
@@ -90,10 +90,11 @@ class ServerNode(Thread):
 
     @staticmethod
     def __get_local_ip_address() -> str:
-        hostname = socket.gethostname()
-        local_ip = socket.gethostbyname(hostname)
-        print(local_ip)
-        return local_ip
+        ip_address = [l for l in (
+        [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [
+            [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in
+             [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+        return ip_address
 
     def __send_heart_beat(self):
         self.time = datetime.now()
