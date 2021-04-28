4b16c8d65be560aa576d0fe82b7402acee398d70
Kai Sun <ksun0017@qq.com>
2021-04-28 14:50:39 +1000

    Update job_manager.py

--- latency_awareness/job_manager.py
+++ latency_awareness/job_manager.py
@@ -29,7 +29,7 @@ class JobManager(Thread):
                 task = self.task_queue.get(timeout=1)
                 file_name = task.script_name
                 print('start_executing:'+file_name)
-                subprocess.Popen(['python', file_name]).wait()
+                subprocess.Popen(['python3', file_name]).wait()
                 if self.log_callback is not None and not task.remote_task:
                     self.log_callback(task)
                 if self.remote_task_execution_finished_callback is not None and task.remote_task:
