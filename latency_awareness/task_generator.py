c3762a8fd5909a4720da23024317a9ccddad1e6a
Kai Sun <ksun0017@qq.com>
2021-05-10 21:03:45 +1000

    Update task_generator.py

--- latency_awareness/task_generator.py
+++ latency_awareness/task_generator.py
@@ -2,6 +2,7 @@ from enum import Enum
 from threading import Thread
 import random
 import time
+from numpy import random
 from datetime import datetime, timedelta
 
 
@@ -10,7 +11,7 @@ class ComputingTask:
     The computing task contains an executable scirpt.
     """
 
-    def __init__(self, id, script_name, deadline=None, except_nodes_id=[], remote_task=False, result_file_name = None):
+    def __init__(self, id, script_name, deadline=None, except_nodes_id=[], remote_task=False, result_file_name=None):
         super().__init__()
         self.id = id
         self.script_name = script_name
@@ -34,26 +35,29 @@ class TaskGenerator(Thread):
     The types of a task will be four types
     """
 
-    def __init__(self, callback):
+    def __init__(self, callback, task_size=1500):
         super().__init__()
         self.callback = callback
         self.task_id = 0
         self.generate_over = False
+        self.task_size = task_size
+        self.deadline = random.poisson(lam=20, size=self.task_size)
+        self.delay = random.poisson(lam=1, size=self.task_size)
 
     def run(self):
-        while self.task_id < 1500:
-            t = self.generate_task()
+        while self.task_id < self.task_size:
+            t = self.generate_task(self.task_id)
             self.task_id += 1
             self.callback(t)
             # time.sleep(random.randint(1,10))
 
             # the interval for generating a task may vary depending on different testing method.
-            time.sleep(random.randint(10, 150) / 1000.0)
+            time.sleep(self.delay[1])
             # time.sleep(random.randint(1, 2) / 1000.0)
         self.generate_over = True
 
-    def generate_task(self) -> ComputingTask:
-        deadline = datetime.now() + timedelta(seconds=random.randint(8, 16))
+    def generate_task(self, id: int) -> ComputingTask:
+        deadline = datetime.now() + timedelta(self.deadline[id])
         seed = 0
         t = ComputingTask(self.task_id, 'image_process_task.py', deadline=deadline,
                           result_file_name='offloading_result.jpg')
@@ -65,6 +69,7 @@ class TaskGenerator(Thread):
 
 
 if __name__ == '__main__':
-    task = ComputingTask(1)
-    task.start()
-    task.join()
+    print(random.poisson(lam=1, size=1000))
+    # task = ComputingTask(1)
+    # task.start()
+    # task.join()
